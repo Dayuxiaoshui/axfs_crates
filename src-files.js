e96383be16a6009d417efa49222530df133ea82e
Dayuxiaoshui <158081477+Dayuxiaoshui@users.noreply.github.com>
2025-06-16 12:04:52 +0000

    Deploying to gh-pages from @ Dayuxiaoshui/axfs_crates@14707c0e4b372a484b24c7abd4cd189fbd824230 🚀

---
+++ src-files.js
@@ -0,0 +1,3 @@
+var srcIndex = new Map(JSON.parse('[["axfs_devfs",["",[],["dir.rs","lib.rs","null.rs","zero.rs"]]],["axfs_ramfs",["",[],["dir.rs","file.rs","lib.rs"]]],["axfs_vfs",["",[],["lib.rs","macros.rs","path.rs","structs.rs"]]]]'));
+createSrcSidebar();
+//{"start":36,"fragment_lengths":[62,53,67]}
